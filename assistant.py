7534857e21cbb3341b9fd2a033ea151226d2c2be
CDXInnova <136953870+CDXInnova@users.noreply.github.com>
2023-09-07 17:58:21 -0500

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -1,16 +1,35 @@
-# Las librerias deben importarse desde la consola o desde línea de comando ejecutando pip install  # noqa: E501
+import sys
 import random
-import speech_recognition as sr
 import openai
+import speech_recognition as sr
 import pyttsx3
 import mysql.connector
 import os
 import subprocess
 from datetime import datetime
-#from geopy.geocoders import Nominatim
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
 
-openai.api_key = "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"
+class Ui(ScreenManager):
+    pass
 
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = 'Dark'
+        self.theme_cls.theme_style = 'Light'
+        Builder.load_file('design.kv')
+        
+        return Ui()
+
+    def change_style(self, checked, value):
+        if value:
+            self.theme_cls.theme_style = 'Dark'
+        else:
+            self.theme_cls.theme_style = 'Light'
+
+
+openai.api_key = "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"
 
 # Funciones relacionadas a la base de datos
 def establecer_conexion():
@@ -24,10 +43,8 @@ def establecer_conexion():
     cnx = mysql.connector.connect(**config)
     return cnx
 
-
 def cerrar_conexion(cnx):
     cnx.close()
-
 
 def insertar_consultoria(cnx, pregunta, respuesta_corta, respuesta_detallada):
     cursor = cnx.cursor()
@@ -37,7 +54,7 @@ def insertar_consultoria(cnx, pregunta, respuesta_corta, respuesta_detallada):
     cursor.execute(query, values)
     cnx.commit()
     cursor.close()
-
+    
 
 def obtener_respuesta(cnx, pregunta, respuesta_type):
     cursor = cnx.cursor()
@@ -53,17 +70,16 @@ def obtener_respuesta(cnx, pregunta, respuesta_type):
     else:
         return "No tengo respuesta para esa pregunta. ¿Deseas que aprenda algo nuevo?"
 
-
+recognizer = sr.Recognizer()
 # Funciones de reconocimiento de voz
 def transformar_audio_a_texto(timeout=15):
-    r = sr.Recognizer()
     with sr.Microphone() as source:
-        r.pause_threshold = 0.8
+        recognizer.adjust_for_ambient_noise(source)
         print("Ya puedes hablar!")
 
         try:
-            audio = r.listen(source, timeout=timeout)
-            pedido = r.recognize_google(audio, language="es-PE")
+            audio = recognizer.listen(source, timeout=timeout)
+            pedido = recognizer.recognize_google(audio, language="es-PE")
             print("You: " + pedido)
             return pedido.lower()
         except sr.WaitTimeoutError:
@@ -79,8 +95,7 @@ def transformar_audio_a_texto(timeout=15):
         except Exception:
             print("Ups, algo salió mal!")
             return "Sigo esperando"
-
-
+        
 # Funciones de síntesis de voz
 def hablar(mensaje):
     engine = pyttsx3.init()
@@ -92,7 +107,6 @@ def hablar(mensaje):
             break
     engine.say(mensaje)
     engine.runAndWait()
-
 
 # Función para verificar y crear el archivo en el directorio actual
 def crear_archivo(archivo):
@@ -115,15 +129,6 @@ def crear_archivo(archivo):
         except Exception as e:
             print("Ocurrió un error al crear el archivo:", str(e))
             return None
-
-
-# Función para abrir el archivo en Visual Studio Code
-def abrir_en_vscode(archivo):
-    archivo_lower = archivo.lower()  # Convertir el nombre del archivo a minúsculas
-    try:
-        subprocess.run(["code", archivo_lower])  # Usar archivo_lower en lugar de archivo  # noqa: E501
-    except Exception as e:
-        print("Ocurrió un error al abrir el archivo en Visual Studio Code:", str(e))
 
 
 def mostrar_categorias():
@@ -167,16 +172,6 @@ def obtener_extension():
         return None
 
 
-def escribir_codigo(codigo, archivo):
-    file_path = os.path.join(os.getcwd(), archivo)
-    try:
-        with open(file_path, 'w') as file:
-            file.write(codigo)
-        return True
-    except Exception as e:
-        print("Ocurrió un error al escribir el código en el archivo:", str(e))
-        return False
-
 
 def crear_archivo_con_categoria():
     mostrar_categorias()
@@ -212,12 +207,39 @@ def crear_archivo_con_categoria():
             hablar("Ocurrió un error al escribir el código en el archivo.")
     else:
         hablar("Ha ocurrido un error al crear el archivo.")
+        
+
+# Función para abrir el archivo en Visual Studio Code
+def abrir_en_vscode(archivo):
+    archivo_lower = archivo.lower()  # Convertir el nombre del archivo a minúsculas
+    try:
+        subprocess.run(["code", archivo_lower])  # Usar archivo_lower en lugar de archiv
+    except Exception as e:
+        print("Ocurrió un error al abrir el archivo en Visual Studio Code:", str(e))
+        
+
+def escribir_codigo(codigo, archivo):
+    file_path = os.path.join(os.getcwd(), archivo)
+    try:
+        with open(file_path, 'w') as file:
+            file.write(codigo)
+        return True
+    except Exception as e:
+        print("Ocurrió un error al escribir el código en el archivo:", str(e))
+        return False
+    
+
+def sugerir_soluciones(codigo):
+    # Llamada a GitHub Copilot para generar sugerencias
+    sugerencias = copilot.generate_code_suggestions(codigo)  # noqa: F821
+    return sugerencias
 
 
 #MOSTRAR HORA MEJORABLE USANDO GEOLOCATOR NOMINATIM#
 def mostrar_hora():
-    current_time = datetime.now().strftime("%H:%M")
-    hablar("La hora actual es " + current_time)
+    now = datetime.now()
+    hora = now.strftime("%H:%M")
+    hablar("La hora actual es " + hora)
 
 
 def hacer_comentario_positivo():
@@ -229,9 +251,23 @@ def hacer_comentario_positivo():
     ]
     comentario_positivo = random.choice(comentarios_positivos)
     hablar(comentario_positivo)
+    
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
 
 
 def main():
+    
+    # Establecer el ícono del programa
+    icon_path = resource_path("icon.ico")
+    if os.path.exists(icon_path):
+        import ctypes
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("myappid")
+        ctypes.windll.kernel32.SetConsoleIcon(0)
+        ctypes.windll.kernel32.SetConsoleIcon(icon_path)
+        
     conversation = ""
     cnx = establecer_conexion()
     last_interaction_time = datetime.now()
@@ -241,7 +277,7 @@ def main():
     while True:
         current_time = datetime.now()
         time_since_last_interaction = current_time - last_interaction_time
-        if time_since_last_interaction.total_seconds() > 150:
+        if time_since_last_interaction.total_seconds() > 1500:
             mostrar_hora()
             hacer_comentario_positivo()
             last_interaction_time = current_time
@@ -294,7 +330,7 @@ def main():
                 model="text-davinci-003",
                 prompt=conversation,
                 temperature=0.5,
-                max_tokens=100,
+                max_tokens=1000,
                 top_p=0.3,
                 frequency_penalty=0.5,
                 presence_penalty=0.0,
@@ -309,4 +345,13 @@ def main():
 
 
 if __name__ == "__main__":
+    MainApp().run()
+    # Establecer el ícono del programa
+    icon_path = resource_path("icon.ico")
+    if os.path.exists(icon_path):
+        
+        import ctypes
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("myappid")
+        ctypes.windll.kernel32.SetConsoleIcon(0)
+        ctypes.windll.kernel32.SetConsoleIcon(icon_path)
     main()
